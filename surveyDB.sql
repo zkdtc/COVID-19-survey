caf4126789ff4748535fcb476f4b05f944acac23
Steve Swan <steve@staffbot.com>
2020-07-16 18:40:05 -0700

    adding schema... still building sequelize js

--- surveyDB.sql
+++ surveyDB.sql
@@ -2,6 +2,11 @@ DROP DATABASE IF EXISTS surveyDB;
 
 CREATE DATABASE surveyDB;
 
+USE surveyDB;
+
+DROP TABLE survey;
+
+
 CREATE TABLE survey (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name	VARCHAR(50)	NULL,
@@ -27,18 +32,31 @@ CREATE TABLE question_order (
 		ON DELETE CASCADE
 );
 
+-- DROP TABLE survey_response;
+
 CREATE TABLE survey_response (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	survey_id	INTEGER,
-		CONSTRAINT fk_survey FOREIGN KEY (survey_id)
-		REFERENCES survey (id)
-		ON DELETE CASCADE,
-	respondent_id	INTEGER,
-		CONSTRAINT fk_respondent FOREIGN KEY (respondent_id)
-		REFERENCES respondent (id)
-		ON DELETE CASCADE,
+	id INT NOT NULL AUTO_INCREMENT,
+-- 	survey_id	INTEGER,
+-- 		CONSTRAINT fk_survey FOREIGN KEY (survey_id)
+-- 		REFERENCES survey (id)
+-- 		ON DELETE CASCADE,
+-- 	respondent_id	INTEGER,
+-- 		CONSTRAINT fk_respondent FOREIGN KEY (respondent_id)
+-- 		REFERENCES respondent (id)
+-- 		ON DELETE CASCADE,
+	PRIMARY KEY (id),
+    FOREIGN KEY (survey_id) REFERENCES survey (id),
+	FOREIGN KEY (respondent_id) REFERENCES respondent (id),
 	upddated	timestamp
     );
+--     
+--     CREATE TABLE Orders (
+--     OrderID int NOT NULL,
+--     OrderNumber int NOT NULL,
+--     PersonID int,
+--     PRIMARY KEY (OrderID),
+--     FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+-- );
 
 CREATE TABLE respondent (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
