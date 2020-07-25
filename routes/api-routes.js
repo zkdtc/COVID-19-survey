dd0a6c202220a1d15d689cf7d4f57a3e912dc50c
zhangkai <zkdtckk@gmail.com>
2020-07-24 22:52:22 -0700

    OK

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
     })
       .then(() => {
         console.log("Good");
-        res.redirect(307, "/api/questions");
+        res.redirect("/api/questions");
       })
       .catch(err => {
         res.status(401).json(err);
